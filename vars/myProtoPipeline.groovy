f0b7a40203c519f1fea27bee4204eeb3ed462b57
Alex Thai <athai@interac.ca>
2019-06-24 12:27:21 -0400

    first commit

---
+++ vars/myProtoPipeline.groovy
@@ -0,0 +1,13 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+
+        stages{
+            stage('checkout git') {
+                steps {
+                    git branch: master, url: pipelineParams.scmUrl
+                }
+            }
+        }
+    }
+}
